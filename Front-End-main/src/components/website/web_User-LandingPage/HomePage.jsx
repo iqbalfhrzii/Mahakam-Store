019795999112e41f6af9a3e93fc35887fcd58c7b
Ardi Dwi Saputra <11221026@student.itk.ac.id>
2025-03-24 23:15:25 +0800

    Add files via upload

--- Front-End-main/src/components/website/web_User-LandingPage/HomePage.jsx
+++ Front-End-main/src/components/website/web_User-LandingPage/HomePage.jsx
@@ -586,7 +586,7 @@ export function HomePage() {
                     type="search"
                     id="location-search"
                     className="block w-full p-4 pl-24 placeholder:pr-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-                    placeholder="Search assets..."
+                    placeholder="Search..."
                     required
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
